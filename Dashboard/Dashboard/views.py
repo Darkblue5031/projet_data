85126a6e9b71be086b4f54a163fc9995a3521bac
kilecon <mathieu.barboteau@ynov.com>
2024-04-11 09:56:47 +0200

    index html

--- Dashboard/Dashboard/views.py
+++ Dashboard/Dashboard/views.py
@@ -11,41 +11,19 @@ def podium(request):
 def location(request):
     return HttpResponse('<h1>location<h1>')
 
+from django.shortcuts import render
+import csv
+
 def index(request):
-    genders = Data.objects.values_list("Gender", flat=True)
-    ages = Data.objects.values_list("Age", flat=True)
-    maritals_status = Data.objects.values_list("Marital_Status", flat=True)
-    occupations = Data.objects.values_list("Occupation", flat=True)
-    Monthly_income = Data.objects.values_list("Monthly_Income", flat=True)
-    Educational_qualification = Data.objects.values_list("Educational_Qualifications", flat=True)
-    latitudes = Data.objects.values_list("Latitude", flat=True)
-    longitudes = Data.objects.values_list("Longitude", flat=True)
-    postal_codes = Data.objects.values_list("Postal_Code", flat=True)
-    feedbacks = Data.objects.values_list("Feedback", flat=True)
-    families_size = Data.objects.values_list("Family_Size", flat=True)
+    # Open the CSV file
+    with open('netflix_titles.csv', newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
 
-    Age_average = Data.Average(Data.Age.field.name)
-    Age_min = Data.Min(Data.Age.field.name)
-    Age_median = Data.Median(Data.objects.values_list("Age", flat=True))
+        # Read the CSV data into a list of dictionaries
+        data = [row for row in reader]
 
-    ratio_male = Data.Ratio_conditional("Gender", "Male")
-    family_3 = Data.Ratio_conditional("Latitude", 3.0, "sup")
+    # Sort the data by the 'title' key
+    sorted_data = sorted(data, key=lambda x: x['title'])
 
-    context = {
-        'genders': genders,
-        'ages': ages,
-        'maritals_status': maritals_status,
-        'occupations': occupations,
-        'monthly_income': Monthly_income,
-        'educational_qualification': Educational_qualification,
-        'families_size': families_size,
-        'latitudes': latitudes,
-        'longitudes': longitudes,
-        'pin_codes': postal_codes,
-        'feedbacks': feedbacks,
-        'family_3': family_3,
-    }
-    print(f"coucou{family_3}")
-    print(f"avearaaaaaaaaaaaaag{Age_average}")
-
-    return render(request, 'html/test.html', context=context)
+    # Pass the sorted data to the template
+    return render(request, 'html/test.html', {'sorted_data': sorted_data})
